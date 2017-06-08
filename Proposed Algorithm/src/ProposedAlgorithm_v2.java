503ba2e77f9c664c0330e7797a033393d159d33e
Md. Abu Raihan Srabon <m.arsrabon@gmail.com>
2017-06-08 09:45:20 +0600

    added a 2nd version of step one.Complete.

--- Proposed Algorithm/src/ProposedAlgorithm_v2.java
+++ Proposed Algorithm/src/ProposedAlgorithm_v2.java
@@ -1,13 +1,9 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
 import java.util.*;
 
 /**
  * Created by msrabon on 08-May-17.
  */
-public class ProposedAlgorithm {
+public class ProposedAlgorithm_v2 {
 
     public static void main(String[] args) {
         List<Minterm> lsb_0 = new ArrayList<>();
@@ -15,18 +11,17 @@ public class ProposedAlgorithm {
         List<List<Minterm>> kMap = new ArrayList<>();
         // patterns holder.
         List<List<Minterm_Group>> patterns = new ArrayList<>();
-//        List<List<Minterm_Group>> patternsFinale = new ArrayList<>();
+        List<List<Minterm_Group>> patternsFinale = new ArrayList<>();
         List<Minterm_Group> pairs = new ArrayList<>();
-//        Map<String, Boolean> minterms = new HashMap<>();
+        Map<String, Boolean> minterms = new HashMap<>();
         Scanner scan = new Scanner(System.in);
-
         boolean dontcaresAvailable;
         int minterm;
         int patternsCount = 0;
         // patterns holder based on position of x.
         for (int i = 0; i < 4; i++) {
             patterns.add(new ArrayList<>());
-//            patternsFinale.add(new ArrayList<>());
+            patternsFinale.add(new ArrayList<>());
         }
 
         System.out.print("Enter minterm: ");
@@ -39,8 +34,8 @@ public class ProposedAlgorithm {
                 lsb_1.add(new Minterm(minterm, String.format("%4s", Integer.toBinaryString(minterm)).replace(" ",
                                                                                                              "0")));
             }
-//            minterms.put(String.format("%4s", Integer.toBinaryString(minterm)).replace(" ",
-//                                                                                       "0"), false);
+            minterms.put(String.format("%4s", Integer.toBinaryString(minterm)).replace(" ",
+                                                                                       "0"), false);
 //            System.out.print("Enter minterm: ");
         }
 
@@ -55,70 +50,56 @@ public class ProposedAlgorithm {
             for (int i = 0; i < lsb.size(); i++) {
                 for (int j = i + 1; j < lsb.size(); j++) {
                     count++;
-                    String min_1 = lsb.get(i).getBit_string();
-                    String min_2 = lsb.get(j).getBit_string();
+                    boolean pair = false;
                     int location = 0;
                     int diff = 0;
-                    for (int k = 0; k < min_1.length(); k++) {
-                        if (min_1.charAt(k) != min_2.charAt(k)) {
-//                        System.out.println(min_1 + "  " + min_2);
-                            diff++;
-                            location = k;
-                        }
+                    int n = 4;
+                    String str = null;
+                    switch (lsb.get(i).getMinterm_no() ^ lsb.get(j).getMinterm_no()) {
+                        case 1: str = getPair(lsb.get(i).getMinterm_no(), n - 1);
+                            pair = true;
+                            location = n - 1;
+                            break;
+                        case 2: str = getPair(lsb.get(i).getMinterm_no(), n - 2);
+                            location = n - 2;
+                            pair = true;
+                            break;
+                        case 4: str = getPair(lsb.get(i).getMinterm_no(), n - 3);
+                            location = n - 3;
+                            pair = true;
+                            break;
+                        case 8: str = getPair(lsb.get(i).getMinterm_no(), n - 4);
+                            location = n - 4;
+                            pair = true;
+                            break;
+                        case 16: str = getPair(lsb.get(i).getMinterm_no(), n - 5);
+                            location = n - 5;
+                            pair = true;
+                            break;
+                        case 32: str = getPair(lsb.get(i).getMinterm_no(), n - 6);
+                            location = n - 6;
+                            pair = true;
+                            break;
                     }
 
-                    if (diff == 1) {
-                        char[] pattern = min_1.toCharArray();
-                        pattern[location] = 'x';
+                    if (pair) {
                         lsb.get(i).setPaired(true);
                         lsb.get(j).setPaired(true);
-//                        minterms.put(lsb.get(i).getBit_string(), true);
-//                        minterms.put(lsb.get(j).getBit_string(), true);
-                        Minterm_Group mintermGroup = new Minterm_Group(String.copyValueOf(pattern));
+                        minterms.remove(lsb.get(i));
+                        minterms.put(lsb.get(i).getBit_string(), true);
+                        minterms.put(lsb.get(j).getBit_string(), true);
+                        Minterm_Group mintermGroup = new Minterm_Group(str);
                         mintermGroup.addToGroupedMinterms(lsb.get(i));
                         mintermGroup.addToGroupedMinterms(lsb.get(j));
                         patterns.get(location).add(mintermGroup);
-                        System.out.println(mintermGroup.getBit_string() + "    " + mintermGroup.getAllMinterms());
                         patternsCount++;
                     }
+                    pair = false;
+                    str = null;
                 }
             }
-
             groupCount--;
         }
-
-//        for (int i = 0; i < lsb.size(); i++) {
-//            List<Minterm> lsb_ = kMap.get(groupCount);
-//            if (!lsb.get(i).isPaired()) {
-//                for (Minterm minterm1 : lsb_) {
-//                    String min_1 = lsb.get(i).getBit_string();
-//                    String min_2 = minterm1.getBit_string();
-//                    int location = 0;
-//                    int diff = 0;
-//                    for (int k = 0; k < min_1.length(); k++) {
-//                        if (min_1.charAt(k) != min_2.charAt(k)) {
-////                        System.out.println(min_1 + "  " + min_2);
-//                            diff++;
-//                            location = k;
-//                        }
-//                    }
-//
-//                    if (diff == 1) {
-//                        char[] pattern = min_1.toCharArray();
-//                        pattern[location] = 'x';
-//                        lsb.get(i).setPaired(true);
-//                        minterm1.setPaired(true);
-//                        minterms.put(lsb.get(i).getBit_string(), true);
-//                        minterms.put(minterm1.getBit_string(), true);
-//                        Minterm_Group mintermGroup = new Minterm_Group(String.copyValueOf(pattern));
-//                        mintermGroup.addToGroupedMinterms(lsb.get(i));
-//                        mintermGroup.addToGroupedMinterms(minterm1);
-//                        patterns.get(location).add(mintermGroup);
-//                        patternsCount++;
-//                    }
-//                }
-//            }
-//        }
 
         System.out.println(count);
         System.out.println("Patterns: " + patternsCount);
@@ -186,5 +167,34 @@ public class ProposedAlgorithm {
 //        }
     }
 
+    private static String getPair(int x, int pos) {
+        char[] pattern = String.format("%4s", Integer.toBinaryString(x)).replace(" ", "0").toCharArray();
+        pattern[pos] = 'x';
+        return String.valueOf(pattern);
+    }
 
+    public static String resultConverter(String bit_string) {
+        String str = "";
+        if (bit_string.charAt(0) == '1') {
+            str += 'A';
+        } else if (bit_string.charAt(0) == '0') {
+            str += 'a';
+        }
+        if (bit_string.charAt(1) == '1') {
+            str += 'B';
+        } else if (bit_string.charAt(1) == '0') {
+            str += 'b';
+        }
+        if (bit_string.charAt(2) == '1') {
+            str += 'C';
+        } else if (bit_string.charAt(2) == '0') {
+            str += 'c';
+        }
+        if (bit_string.charAt(3) == '1') {
+            str += 'D';
+        } else if (bit_string.charAt(3) == '0') {
+            str += 'd';
+        }
+        return str;
+    }
 }
---
+++ Proposed Algorithm/src/ShowResult.java
@@ -0,0 +1,44 @@
+import java.util.List;
+
+/**
+ * Created by msrabon on 07-Jun-17.
+ */
+public class ShowResult {
+
+    public static void showTruthtable() {
+        for (int i = 0; i < 16; i++) {
+            System.out.println(String.format("%4s", Integer.toBinaryString(i)).replace(" ", "0"));
+        }
+    }
+
+    public static void showPairs(List<Minterm_Group> groups) {
+        for (Minterm_Group group : groups) {
+            System.out.println(resultConverter(group.getBit_string()) + "  " + group.getAllMinterms());
+        }
+    }
+
+    public static String resultConverter(String bit_string) {
+        String str = "";
+        if (bit_string.charAt(0) == '1') {
+            str += 'A';
+        } else if (bit_string.charAt(0) == '0') {
+            str += 'a';
+        }
+        if (bit_string.charAt(1) == '1') {
+            str += 'B';
+        } else if (bit_string.charAt(1) == '0') {
+            str += 'b';
+        }
+        if (bit_string.charAt(2) == '1') {
+            str += 'C';
+        } else if (bit_string.charAt(2) == '0') {
+            str += 'c';
+        }
+        if (bit_string.charAt(3) == '1') {
+            str += 'D';
+        } else if (bit_string.charAt(3) == '0') {
+            str += 'd';
+        }
+        return str;
+    }
+}
